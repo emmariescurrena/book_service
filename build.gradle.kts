fa3d00a4e90cfa216d54c818604259eedd683b6c
emmariescurrena <emmariescurrena>
2024-11-17 14:53:00 -0300

    Adds validation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
