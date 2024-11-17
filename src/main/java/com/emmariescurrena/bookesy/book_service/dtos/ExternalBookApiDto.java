b662e1c89277856e599a8c614a5be053102e352d
emmariescurrena <emmariescurrena>
2024-11-17 15:00:11 -0300

    Creates ExternalBookApiDto

---
+++ src/main/java/com/emmariescurrena/bookesy/book_service/dtos/ExternalBookApiDto.java
@@ -0,0 +1,19 @@
+package com.emmariescurrena.bookesy.book_service.dtos;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public abstract class ExternalBookApiDto {
+    
+    public abstract String getBookId();
+    public abstract String getTitle();
+    public abstract String getDescription();
+    public abstract LocalDate getPublishDate();
+    public abstract List<String> getGenres();
+    public abstract List<String> getAuthorsIds();
+    public abstract Integer getCoverId();
+
+}
