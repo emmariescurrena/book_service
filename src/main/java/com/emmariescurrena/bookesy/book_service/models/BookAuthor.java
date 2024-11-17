789359c3596db7852a29488fc19fabf884ee888c
emmariescurrena <emmariescurrena>
2024-11-17 14:54:13 -0300

    Creates book-author model

---
+++ src/main/java/com/emmariescurrena/bookesy/book_service/models/BookAuthor.java
@@ -0,0 +1,36 @@
+package com.emmariescurrena.bookesy.book_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BookAuthor {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id", nullable = false)
+    private Author author;
+
+    public BookAuthor(Book book, Author author) {
+        this.book = book;
+        this.author = author;
+    }
+
+}
