444fc81a9bea6ad356cde8acf1d4ea12c3bd3703
emmariescurrena <emmariescurrena>
2024-11-17 14:58:32 -0300

    Creates BookAuthorRepository

---
+++ src/main/java/com/emmariescurrena/bookesy/book_service/repositories/BookAuthorRepository.java
@@ -0,0 +1,15 @@
+package com.emmariescurrena.bookesy.book_service.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emmariescurrena.bookesy.book_service.models.BookAuthor;
+
+
+@Repository
+public interface BookAuthorRepository extends JpaRepository<BookAuthor, Long> {
+    List<BookAuthor> findByAuthorId(String authorId);
+    List<BookAuthor> findByBookId(String bookId);
+}
