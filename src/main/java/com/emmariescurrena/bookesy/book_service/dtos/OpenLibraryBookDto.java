646149764b44c6b92f340bf8ac959190dc786acf
emmariescurrena <emmariescurrena>
2024-11-19 08:38:37 -0300

    Adds description and subtitle handling

--- src/main/java/com/emmariescurrena/bookesy/book_service/dtos/OpenLibraryBookDto.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/dtos/OpenLibraryBookDto.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,30 +20,50 @@ public class OpenLibraryBookDto extends ExternalBookApiDto {
 
     private String title;
 
+    private String subtitle;
+
     private String description;
-    
+
     @JsonProperty("first_publish_date")
     private LocalDate publishDate;
 
     private List<Integer> covers;
-    
+
     @JsonProperty("subjects")
     private List<String> genres;
-    
+
     private List<Map<String, String>> authors;
 
     @Override
-    public List<String> getAuthorsIds() {
-        List<String> authorsIds = new ArrayList<>();
-        for (Map<String, String> author: authors) {
-            authorsIds.add(author.get("key"));
+    public String getTitle() {
+        return subtitle != null && !subtitle.isEmpty() ? title + ": " + subtitle : title;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(JsonNode descriptionNode) {
+        if (descriptionNode != null) {
+            if (descriptionNode.isTextual()) {
+                this.description = descriptionNode.asText();
+            } else if (descriptionNode.has("value")) {
+                this.description = descriptionNode.get("value").asText();
+            }
+        } else {
+            this.description = "No description available";
         }
-        return authorsIds;
     }
 
     @Override
     public Integer getCoverId() {
-        return covers.get(0);
+        return covers != null && !covers.isEmpty() ? covers.get(0) : null;
+    }
+
+    @Override
+    public List<String> getAuthorsIds() {
+        List<String> authorsIds = new ArrayList<>();
+        for (Map<String, String> author : authors) {
+            authorsIds.add(author.get("key"));
+        }
+        return authorsIds;
     }
 
 }
