1edff648693d2c2ec555047e365830412864d17f
emmariescurrena <emmariescurrena>
2024-11-22 09:49:32 -0300

    Removes photosIds and getPhotoId and adds getId, setId, photoUrl, getPhotoUrl and setPhotoUrl

--- src/main/java/com/emmariescurrena/bookesy/book_service/dtos/OpenLibraryAuthorDto.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/dtos/OpenLibraryAuthorDto.java
@@ -2,6 +2,7 @@ package com.emmariescurrena.bookesy.book_service.dtos;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -11,17 +12,38 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class OpenLibraryAuthorDto extends ExternalAuthorApiDto {
 
-    @JsonProperty("key")
     private String id;
 
     private String name;
 
-    @JsonProperty("photos")
-    private List<Integer> photosIds;
+    @JsonProperty("photoUrl")
+    private String photoUrl;
 
-    @Override
-    public Integer getPhotoId() {
-        return photosIds.get(0);
+    @JsonIgnore
+    public String getId() {
+        return id;
     }
 
+    @JsonProperty("key")
+    public void setId(String key) {
+        this.id = key.substring("/authors/".length());
+    }
+
+    @JsonIgnore
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    @JsonProperty("photos")
+    public void setPhotoUrl(List<Integer> photos) {
+        if (photos == null || photos.isEmpty()) {
+            this.photoUrl = null;
+        } else {
+            this.photoUrl = String.format(
+                "https://covers.openlibrary.org/a/id/%d-M.jpg/",
+                photos.get(0)
+            );
+        }
+    }
 }
+
