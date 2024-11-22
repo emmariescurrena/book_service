cc3ec05db6fb06f9af53c253926a2811dbfdb361
emmariescurrena <emmariescurrena>
2024-11-22 09:47:10 -0300

    Makes BookTransactionService reactive, removes saveBookWithDetails and adds findOrSaveBooks, findOrSaveBook and buildBookDetailsDto

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/BookTransactionService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/BookTransactionService.java
@@ -2,13 +2,17 @@ package com.emmariescurrena.bookesy.book_service.services;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.emmariescurrena.bookesy.book_service.dtos.BookDetailsDto;
 import com.emmariescurrena.bookesy.book_service.models.Author;
 import com.emmariescurrena.bookesy.book_service.models.Book;
-import com.emmariescurrena.bookesy.book_service.models.Genre;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BookTransactionService {
@@ -23,27 +27,30 @@ public class BookTransactionService {
     GenreService genreService;
     
     @Autowired
-    BookGenreService bookGenreService;
-    
-    @Autowired
     BookAuthorService bookAuthorService;
 
+    @Autowired
+    OpenLibraryService openLibraryService;
+
     @Transactional
-    public void saveBookWithDetails(Book book, List<Author> authors, List<Genre> genres) {
-        // Save the book
-        Book savedBook = bookService.saveBook(book);
+    public Flux<BookDetailsDto> findOrSaveBooks(List<String> bookIds) {
+        return Flux.fromIterable(bookIds)
+                    .flatMap(this::findOrSaveBook);
+    }
 
-        // Save authors and link to book
-        for (Author author : authors) {
-            Author savedAuthor = authorService.findOrSaveAuthor(author);
-            bookAuthorService.linkBookAndAuthor(savedBook, savedAuthor);
-        }
+    private Mono<BookDetailsDto> findOrSaveBook(String bookId) {
+        return bookService.findOrSaveBook(bookId)
+                          .flatMap(book -> bookAuthorService.getAuthorsByBookId(book.getId())
+                                                            .collectList()
+                                                            .map(authors -> buildBookDetailsDto(book, authors)));
+    }
+    
 
-        // Save genres and link to book
-        for (Genre genre : genres) {
-            Genre savedGenre = genreService.findOrSaveGenre(genre);
-            bookGenreService.linkBookAndGenre(savedBook, savedGenre);
-        }
+    private BookDetailsDto buildBookDetailsDto(Book book, List<Author> authors) {
+        BookDetailsDto bookDetailsDto = new BookDetailsDto();
+        BeanUtils.copyProperties(book, bookDetailsDto);
+        bookDetailsDto.setAuthors(authors);
+        return bookDetailsDto;
     }
 
 }
