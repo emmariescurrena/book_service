d608bf0f57e8f020b38046325c4bbf79c6a2ac6b
emmariescurrena <emmariescurrena>
2024-11-22 09:36:27 -0300

    Creates BookDetailsDto

---
+++ src/main/java/com/emmariescurrena/bookesy/book_service/dtos/BookDetailsDto.java
@@ -0,0 +1,37 @@
+package com.emmariescurrena.bookesy.book_service.dtos;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.emmariescurrena.bookesy.book_service.models.Author;
+import com.emmariescurrena.bookesy.book_service.models.Genre;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class BookDetailsDto {
+    
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "The title is required")
+    private String title;
+
+    private String subtitle;
+
+    @Column(nullable = false)
+    private String description;
+
+    private LocalDate publishDate;
+
+    private String coverUrl;
+
+    private List<Author> authors;
+
+    private List<Genre> genres;
+
+}
