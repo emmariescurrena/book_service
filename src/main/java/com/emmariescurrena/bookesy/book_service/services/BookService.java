24e99f32646ee56d887684884ba194bf3d83b8f3
emmariescurrena <emmariescurrena>
2024-11-23 11:26:36 -0300

    Adds caching and makes service reactive

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/BookService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/BookService.java
@@ -1,12 +1,17 @@
 package com.emmariescurrena.bookesy.book_service.services;
 
-import java.util.Optional;
+import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.emmariescurrena.bookesy.book_service.models.Book;
 import com.emmariescurrena.bookesy.book_service.repositories.BookRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class BookService {
@@ -14,12 +19,39 @@ public class BookService {
     @Autowired
     BookRepository bookRepository;
 
-    public Book saveBook(Book book) {
-        return bookRepository.save(book);
-    }
+    @Autowired
+    OpenLibraryService openLibraryService;
 
-    public Optional<Book> getBookById(String id) {
-        return bookRepository.findById(id);
+    @Autowired
+    AuthorService authorService;
+
+    @Autowired
+    GenreService genreService;
+
+    @Autowired
+    BookAuthorService bookAuthorService;
+
+
+    @Cacheable("books")
+    public Mono<Book> findOrSaveBook(String bookId) {
+        return Mono.justOrEmpty(bookRepository.findById(bookId))
+            .switchIfEmpty(
+                openLibraryService.getBook(bookId)
+                    .flatMap(externalBookApiDto -> {
+                        Book book = new Book();
+                        BeanUtils.copyProperties(externalBookApiDto, book, "genres", "authorsIds");
+
+                        return Flux.fromIterable(externalBookApiDto.getGenres() == null ? List.of() : externalBookApiDto.getGenres())
+                                    .flatMap(genreService::findOrSaveGenre)
+                                    .collectList()
+                                    .flatMap(genres -> {
+                                        book.setGenres(genres);
+                                        return Mono.fromCallable(() -> bookRepository.save(book))
+                                            .doOnNext(savedBook -> 
+                                                bookAuthorService.linkAuthorsToBook(savedBook, externalBookApiDto.getAuthorsIds()));
+                                    });
+                    })
+            );
     }
 
     public void deleteBook(Book book) {
