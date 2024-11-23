0d375aef15ad81d8e90505084908be1d323ca011
emmariescurrena <emmariescurrena>
2024-11-23 11:27:05 -0300

    Makes service reactive and removes getAuthorById

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/AuthorService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/AuthorService.java
@@ -1,26 +1,34 @@
 package com.emmariescurrena.bookesy.book_service.services;
 
-import java.util.Optional;
-
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emmariescurrena.bookesy.book_service.models.Author;
 import com.emmariescurrena.bookesy.book_service.repositories.AuthorRepository;
 
+import reactor.core.publisher.Mono;
+
 @Service
 public class AuthorService {
-    
+
     @Autowired
     AuthorRepository authorRepository;
 
-    public Author findOrSaveAuthor(Author author) {
-        return authorRepository.findById(author.getId())
-                .orElseGet(() -> authorRepository.save(author));
-    }
+    @Autowired
+    OpenLibraryService openLibraryService;
 
-    public Optional<Author> getAuthorById(String id) {
-        return authorRepository.findById(id);
+    public Mono<Author> findOrSaveAuthor(String authorId) {
+
+        return Mono.justOrEmpty(authorRepository.findById(authorId))
+            .switchIfEmpty(
+                openLibraryService.getAuthor(authorId)
+                                .map(externalAuthorApiDto -> {
+                                    Author author = new Author();
+                                    BeanUtils.copyProperties(externalAuthorApiDto, author);
+                                    return authorRepository.save(author);
+                                }));
+
     }
 
 }
