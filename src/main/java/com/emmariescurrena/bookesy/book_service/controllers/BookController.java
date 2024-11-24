81a5ae317350fe4e672d446113e783fc3153b3c8
emmariescurrena <emmariescurrena>
2024-11-24 07:27:19 -0300

    Removes getBook and getAuthor

--- src/main/java/com/emmariescurrena/bookesy/book_service/controllers/BookController.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/controllers/BookController.java
@@ -2,21 +2,17 @@ package com.emmariescurrena.bookesy.book_service.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emmariescurrena.bookesy.book_service.dtos.BookDetailsDto;
-import com.emmariescurrena.bookesy.book_service.models.Author;
-import com.emmariescurrena.bookesy.book_service.models.Book;
 import com.emmariescurrena.bookesy.book_service.services.AuthorService;
 import com.emmariescurrena.bookesy.book_service.services.BookService;
 import com.emmariescurrena.bookesy.book_service.services.BookTransactionService;
 import com.emmariescurrena.bookesy.book_service.services.OpenLibraryService;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 
 
@@ -41,16 +37,6 @@ public class BookController {
         return openLibraryService.searchBooksIds(query, page)
                                 .collectList()
                                 .flatMapMany(bookTransactionService::findOrSaveBooks);
-    }
-
-    @GetMapping("/books/{bookId}")
-    public Mono<Book> getBook(@PathVariable String bookId) {
-        return bookService.findOrSaveBook(bookId);
-    }
-
-    @GetMapping("/authors/{authorId}")
-    public Mono<Author> getAuthor(@PathVariable String authorId) {
-        return authorService.findOrSaveAuthor(authorId);
     }
 
 }
