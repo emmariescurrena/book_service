010fecbe1f26e9934fed0040ca15eaf3d2b45d76
emmariescurrena <emmariescurrena>
2025-01-03 09:14:25 -0300

    Refactors findOrSaveAuthor and adds findAuthor

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/AuthorService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/AuthorService.java
@@ -1,9 +1,9 @@
 package com.emmariescurrena.bookesy.book_service.services;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emmariescurrena.bookesy.book_service.exceptions.NotFoundException;
 import com.emmariescurrena.bookesy.book_service.models.Author;
 import com.emmariescurrena.bookesy.book_service.repositories.AuthorRepository;
 
@@ -19,16 +19,21 @@ public class AuthorService {
     OpenLibraryService openLibraryService;
 
     public Mono<Author> findOrSaveAuthor(String authorId) {
-
-        return Mono.justOrEmpty(authorRepository.findById(authorId))
+        return authorRepository.findById(authorId)
             .switchIfEmpty(
                 openLibraryService.getAuthor(authorId)
-                                .map(externalAuthorApiDto -> {
-                                    Author author = new Author();
-                                    BeanUtils.copyProperties(externalAuthorApiDto, author);
-                                    return authorRepository.save(author);
-                                }));
+                    .flatMap(externalAuthorApiDto -> {
+                        return authorRepository.saveIfNotExists(
+                            externalAuthorApiDto.getId(),
+                            externalAuthorApiDto.getName()
+                        );
+                    })
+            );
+    }
 
+    public Mono<Author> findAuthor(String authorId) {
+        return authorRepository.findById(authorId)
+            .switchIfEmpty(Mono.error(new NotFoundException(String.format("Author with %s id was not found", authorId))));
     }
 
 }
