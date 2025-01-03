dacc64c3f368b4df204d1dd25d68cf02bc657fb3
emmariescurrena <emmariescurrena>
2025-01-03 09:10:40 -0300

    Adds saveIfNotExists

--- src/main/java/com/emmariescurrena/bookesy/book_service/repositories/AuthorRepository.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/repositories/AuthorRepository.java
@@ -1,11 +1,16 @@
 package com.emmariescurrena.bookesy.book_service.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.bookesy.book_service.models.Author;
 
+import reactor.core.publisher.Mono;
+
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, String> {
-    
+public interface AuthorRepository extends ReactiveCrudRepository<Author, String> {
+    @Query("INSERT INTO authors (id, name) VALUES (:id, :name) ON CONFLICT (id) DO NOTHING RETURNING *")
+    Mono<Author> saveIfNotExists(@Param("id") String id, @Param("name") String name);
 }
