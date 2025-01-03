933ad5451d157b606d71407d85dd65c6b9f31a72
emmariescurrena <emmariescurrena>
2025-01-03 09:13:40 -0300

    Extends ReactiveCrudRepository, adds findByName and saveIfNotExists

--- src/main/java/com/emmariescurrena/bookesy/book_service/repositories/GenreRepository.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/repositories/GenreRepository.java
@@ -1,11 +1,18 @@
 package com.emmariescurrena.bookesy.book_service.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.bookesy.book_service.models.Genre;
 
-@Repository
-public interface GenreRepository extends JpaRepository<Genre, String> {
+import reactor.core.publisher.Mono;
 
+@Repository
+public interface GenreRepository extends ReactiveCrudRepository<Genre, String> {
+    Mono<Genre> findByName(String name);
+
+    @Query("INSERT INTO genres (name) VALUES (:name) ON CONFLICT (name) DO NOTHING RETURNING *")
+    Mono<Genre> saveIfNotExists(@Param("name") String name);
 }
