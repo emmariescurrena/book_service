7c00193ec6db3e0084bcd8e446eb105f80fbd212
emmariescurrena <emmariescurrena>
2025-01-03 09:13:16 -0300

    Extends ReactiveCrudRepository

--- src/main/java/com/emmariescurrena/bookesy/book_service/repositories/BookRepository.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/repositories/BookRepository.java
@@ -1,11 +1,11 @@
 package com.emmariescurrena.bookesy.book_service.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.bookesy.book_service.models.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, String> {
+public interface BookRepository extends ReactiveCrudRepository<Book, String> {
     
 }
