23684710f2f738f7e5791ea3c66161b847b8c767
emmariescurrena <emmariescurrena>
2025-01-03 09:12:45 -0300

    Extends ReactiveCrudRepository, removes findByBookId and adds findAuthorsIdsByBookId

--- src/main/java/com/emmariescurrena/bookesy/book_service/repositories/BookAuthorRepository.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/repositories/BookAuthorRepository.java
@@ -1,14 +1,18 @@
 package com.emmariescurrena.bookesy.book_service.repositories;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.bookesy.book_service.models.BookAuthor;
 
+import reactor.core.publisher.Flux;
+
 @Repository
-public interface BookAuthorRepository extends JpaRepository<BookAuthor, Long> {
-    List<BookAuthor> findByAuthorId(String authorId);
-    List<BookAuthor> findByBookId(String bookId);
+public interface BookAuthorRepository extends ReactiveCrudRepository<BookAuthor, Long> {
+    Flux<BookAuthor> findByAuthorId(String authorId);
+
+    @Query("SELECT author_id FROM books_authors WHERE book_id = :bookId")
+    Flux<String> findAuthorsIdsByBookId(@Param("bookId") String bookId);
 }
