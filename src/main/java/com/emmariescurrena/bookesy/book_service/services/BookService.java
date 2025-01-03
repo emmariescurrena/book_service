15811a32b712a7a0daacf71eec05a089b234a7e6
emmariescurrena <emmariescurrena>
2025-01-03 09:18:50 -0300

    Refactors flow logic in Book Service

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/BookService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/BookService.java
@@ -1,17 +1,14 @@
 package com.emmariescurrena.bookesy.book_service.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.emmariescurrena.bookesy.book_service.dtos.BookSearchResultDto;
 import com.emmariescurrena.bookesy.book_service.exceptions.NotFoundException;
 import com.emmariescurrena.bookesy.book_service.models.Book;
-import com.emmariescurrena.bookesy.book_service.models.Genre;
 import com.emmariescurrena.bookesy.book_service.repositories.BookRepository;
 
 import reactor.core.publisher.Flux;
@@ -21,68 +18,73 @@ import reactor.core.publisher.Mono;
 public class BookService {
 
     @Autowired
-    BookRepository bookRepository;
+    private BookRepository bookRepository;
 
     @Autowired
-    OpenLibraryService openLibraryService;
+    private OpenLibraryService openLibraryService;
 
     @Autowired
-    AuthorService authorService;
+    private AuthorService authorService;
 
     @Autowired
-    GenreService genreService;
+    private GenreService genreService;
 
     @Autowired
-    BookAuthorService bookAuthorService;
+    private BookAuthorService bookAuthorService;
+
+    @Autowired
+    private BookGenreService bookGenreService;
 
 
-    @Cacheable("books")
     public Mono<Book> findOrSaveBook(BookSearchResultDto bookSearchResultDto) {
-        return Mono.justOrEmpty(bookRepository.findById(bookSearchResultDto.getBookId()))
+        return bookRepository.findById(bookSearchResultDto.getBookId())
             .switchIfEmpty(
                 openLibraryService.getBook(bookSearchResultDto.getBookId())
                     .flatMap(externalBookApiDto -> {
                         Book book = new Book();
                         BeanUtils.copyProperties(externalBookApiDto, book, "genres", "authorsIds");
-
                         if (bookSearchResultDto.getPublishedYear() != null) {
                             book.setPublishedYear(bookSearchResultDto.getPublishedYear());
                         }
 
-                        Mono<List<Genre>> genresMono = Flux.fromIterable(
-                                Optional.ofNullable(externalBookApiDto.getGenres()).orElse(List.of()))
-                            .flatMap(genreService::findOrSaveGenre)
-                            .collectList();
+                        return bookRepository.save(book)
+                            .flatMap(savedBook ->
+                                linkAuthorsToBook(savedBook, externalBookApiDto.getAuthorsIds())
+                                    .then(linkGenresToBook(savedBook, externalBookApiDto.getGenres()))
+                                    .thenReturn(savedBook)
+                            );
 
-                        return genresMono.flatMap(genres -> {
-                            book.setGenres(genres);
-                            return Mono.defer(() -> Mono.just(bookRepository.save(book)))
-                                .flatMap(savedBook -> {
-                                    return linkAuthorsToBook(savedBook, externalBookApiDto.getAuthorsIds());
-                                });
-                        });
                     })
             );
     }
 
-    private Mono<Book> linkAuthorsToBook(Book book, List<String> authorsIds) {
+    private Mono<Void> linkAuthorsToBook(Book book, List<String> authorsIds) {
         if (authorsIds == null || authorsIds.isEmpty()) {
-            return Mono.just(book);
+            return Mono.empty();
         }
 
         return Flux.fromIterable(authorsIds)
-            .flatMap(authorService::findOrSaveAuthor)
-            .doOnNext(author -> bookAuthorService.linkBookAndAuthor(book, author))
-            .then(Mono.just(book));
+        .flatMap(authorService::findOrSaveAuthor)
+        .flatMap(author -> bookAuthorService.linkBookAndAuthor(book, author))
+        .then();
+    }
+
+    private Mono<Void> linkGenresToBook(Book book, List<String> genresNames) {
+        if (genresNames == null || genresNames.isEmpty()) {
+            return Mono.empty();
+        }
+
+
+
+        return Flux.fromIterable(genresNames)
+        .flatMap(genreService::findOrSaveGenre)
+        .flatMap(genre -> bookGenreService.linkBookAndGenre(book, genre))
+        .then();
     }
 
     public Mono<Book> findBook(String bookId) {
-        Optional<Book> optionalBook = bookRepository.findById(bookId);
-        if (optionalBook.isEmpty()) {
-            throw new NotFoundException(String.format("Book with %s id was not found", bookId));
-        }
-
-        return Mono.just(optionalBook.get());
+        return bookRepository.findById(bookId)
+            .switchIfEmpty(Mono.error(new NotFoundException(String.format("Book with %s id was not found", bookId))));
     }
 
     public void deleteBook(Book book) {
