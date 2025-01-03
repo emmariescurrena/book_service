6108261c8b3d3831b9791caf4670eaaf75bd5a42
emmariescurrena <emmariescurrena>
2025-01-03 09:08:29 -0300

    Adds @EnableDiscoveryClient and removes @EnableCaching

--- src/main/java/com/emmariescurrena/bookesy/book_service/BookServiceApplication.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/BookServiceApplication.java
@@ -2,10 +2,10 @@ package com.emmariescurrena.bookesy.book_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableCaching
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
