6abb9fbb3415ae5fe0f29acb271d3e2c6813ac82
emmariescurrena <emmariescurrena>
2025-01-03 09:14:51 -0300

    Refactors findOrSaveGenre and adds findGenre

--- src/main/java/com/emmariescurrena/bookesy/book_service/services/GenreService.java
+++ src/main/java/com/emmariescurrena/bookesy/book_service/services/GenreService.java
@@ -1,10 +1,9 @@
 package com.emmariescurrena.bookesy.book_service.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emmariescurrena.bookesy.book_service.exceptions.NotFoundException;
 import com.emmariescurrena.bookesy.book_service.models.Genre;
 import com.emmariescurrena.bookesy.book_service.repositories.GenreRepository;
 
@@ -17,16 +16,15 @@ public class GenreService {
     GenreRepository genreRepository;
 
     public Mono<Genre> findOrSaveGenre(String genreName) {
-        return Mono.defer(() -> {
-            Optional<Genre> optionalGenre = genreRepository.findById(genreName);
-            if (optionalGenre.isPresent()) {
-                return Mono.just(optionalGenre.get());
-            }
+        return genreRepository.findByName(genreName)
+                .switchIfEmpty(
+                    genreRepository.saveIfNotExists(genreName)
+                );
+    }
 
-            Genre genre = new Genre();
-            genre.setName(genreName);
-            return Mono.fromCallable(() -> genreRepository.save(genre));
-        });
+    public Mono<Genre> findGenre(String genreId) {
+        return genreRepository.findById(genreId)
+            .switchIfEmpty(Mono.error(new NotFoundException(String.format("Author with %s id was not found", genreId))));
     }
 
 }
